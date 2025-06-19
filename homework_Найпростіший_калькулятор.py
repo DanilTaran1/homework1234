1ce7dfc3798dc88cc77a959e21bbaf478349a43b
danil t <daniltaran2001@gmail.com>
2025-06-19 21:51:29 +0200

    first_task

--- homework_Найпростіший_калькулятор.py
+++ homework_Найпростіший_калькулятор.py
@@ -2,17 +2,16 @@ number_1 = int(input("Введите число: "))
 number_2 = int(input("Введите второе число: "))
 action = input("Введите знак действия которое хотите выполнить: ")
 if action == "+":
-    result = number_1+number_2
-    print (result)
-elif action == "-":
-    result = number_1-number_2
-    print (result)
-elif action == "*":
-    result = number_1*number_2
-    print (result)
-elif action == "/":
-    result = number_1/number_2
+    result = number_1 + number_2
     print(result)
+elif action == "-":
+    result = number_1 - number_2
+    print(result)
+elif action == "*":
+    result = number_1 * number_2
+    print(result)
+elif action == "/" and number_2 == 0:
+    print("Недопустимое значение (деление на ноль)")
 else:
-    action == '%'
-    print ("Недопустимое значение")
+    result = number_1 / number_2
+    print(result)

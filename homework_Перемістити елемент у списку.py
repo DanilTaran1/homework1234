1987f4977b7d56c46463f807f0bf3a582dfa859a
danil t <daniltaran2001@gmail.com>
2025-06-22 17:00:14 +0200

    first_task

--- homework_Перемістити елемент у списку.py
+++ homework_Перемістити елемент у списку.py
@@ -1,9 +1,7 @@
-lst = [12, 3, 4 ,10]
-lst_2 = [3,6,1,5,7]
-lst_3 = [10,9,8,7]
-a  = lst[::-1]
-b = lst_2[::-1]
-c = lst_3[::-1]
-print (a)
-print (b)
-print (c)
+lst = [12, 3, 4, 10]
+if lst == []:
+    print (lst)
+else:
+    removed_a = lst.pop (-1)
+    lst.insert(0,removed_a)
+    print (lst)

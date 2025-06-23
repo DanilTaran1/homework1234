183beb3f60dd3de2118fa4e526c6cb90710395dc
danil t <daniltaran2001@gmail.com>
2025-06-23 10:45:33 +0200

    first_task

--- homework_Розділити один список на два списки.py
+++ homework_Розділити один список на два списки.py
@@ -3,6 +3,6 @@ if len(lst) == 1:
     result = [lst ,[]]
     print(result)
 else:
-    cutted = len(lst) // 2
-    print (cutted)
+    cutted = len(lst)// 2
+    print(cutted)
     print([lst[:-cutted], lst[-cutted:]])

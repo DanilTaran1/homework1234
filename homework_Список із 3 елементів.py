514106950956b49ae3eff36dc772d2d898097f26
danil t <daniltaran2001@gmail.com>
2025-06-26 18:24:47 +0200

    first_task

--- homework_Список із 3 елементів.py
+++ homework_Список із 3 елементів.py
@@ -3,16 +3,13 @@ lst = [random.randint(1,10) for i in range(random.randint(3, 10))]
 #lst = [1,2,3]
 if len(lst) == 3:
     first = lst.pop(2)
-    second = lst.pop(-2)
-    third = lst.pop(0)
-    newlst = [first,second,third]
-    newlst.sort()
+    second = lst.pop(0)
+    third = lst.pop(-1)
+    newlst = [second,first,third]
     print(newlst)
 else:
     a = lst.pop(2)
     b = lst.pop(0)
     c = lst.pop(-2)
-    new_lst = [a,b,c]
-    new_lst.sort()
+    new_lst = [b,a,c]
     print (new_lst)
-#print(lst)

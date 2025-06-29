1310a82a444b4a0b9e857d775ce2a019f03c9b8e
danil t <daniltaran2001@gmail.com>
2025-06-29 14:18:27 +0200

    first_task

--- homework_Найпростіший_калькулятор.py
+++ homework_Найпростіший_калькулятор.py
@@ -1,17 +1,26 @@
-number_1 = int(input("Введите число: "))
-number_2 = int(input("Введите второе число: "))
-action = input("Введите знак действия которое хотите выполнить: ")
-if action == "+":
-    result = number_1 + number_2
-    print(result)
-elif action == "-":
-    result = number_1 - number_2
-    print(result)
-elif action == "*":
-    result = number_1 * number_2
-    print(result)
-elif action == "/" and number_2 == 0:
-    print("Недопустимое значение (деление на ноль)")
-else:
-    result = number_1 / number_2
-    print(result)
+while True:
+    number_1 = int(input("Введите число: "))
+    number_2 = int(input("Введите второе число: "))
+    action = input("Введите знак действия которое хотите выполнить: ")
+    if action == "+":
+        result = number_1 + number_2
+        print(result)
+    elif action == "-":
+        result = number_1 - number_2
+        print(result)
+    elif action == "*":
+        result = number_1 * number_2
+        print(result)
+    elif action == "/" and number_2 == 0:
+        print("Недопустимое значение (деление на ноль)")
+    else:
+        result = number_1 / number_2
+        print(result)
+    print("Продолжить работу?")
+    answer = input()
+    if answer == "yes" or answer == "y":
+        continue
+    elif answer == "no" or answer == "n":
+        break
+
+

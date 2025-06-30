360b8ce474a65b418e4d027fcb860bfa4027f74d
danil t <daniltaran2001@gmail.com>
2025-06-30 11:30:24 +0200

    first_task

--- homework_hashtag.py
+++ homework_hashtag.py
@@ -1,4 +1,9 @@
-my_string = "i like python"
-my_string = my_string.title()
-string = "".join(my_string)
-print(string)
+import string
+my_string = input("Enter a string: ")
+for i in string.punctuation:
+    my_string = my_string.replace(i, "")
+my_string_1 = my_string.title()
+my_string_2 = my_string_1.replace(" ", "")
+my_string_3 = "#" + my_string_2
+my_string_4 = my_string_3[:140]
+print(my_string_4)

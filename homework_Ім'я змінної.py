14f02ef73f5511532f3374e7a4d10381554eb0a0
danil t <daniltaran2001@gmail.com>
2025-06-30 16:24:28 +0200

    first_task

--- homework_Ім'я змінної.py
+++ homework_Ім'я змінної.py
@@ -2,4 +2,10 @@ import string
 import keyword
 my_string = input("Enter a string: ")
 if my_string[0].isdigit():
-
+    print("False")
+if any(my_string.isupper() for my_string in my_string):
+    print("False")
+if my_string in keyword.kwlist:
+    print("False")
+if any(my_string for i in string.punctuation):
+    print("False")
